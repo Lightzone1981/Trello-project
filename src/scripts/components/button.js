cad5ff2f29a00ee7e3debddcfec4c7d79861e637
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2023-02-06 20:15:22 +0300

    Delete src directory

--- src/scripts/components/button.js
+++
@@ -1,8 +0,0 @@
-export function createButton (id, className, text, type) {
-  const button = document.createElement('button')
-  button.className = className
-  button.id = id
-  button.textContent = text
-  button.type = type
-  return button
-}
--- src/scripts/index.js
+++
@@ -1,2 +0,0 @@
-import '../styles/index.scss'
-console.log('hello')
