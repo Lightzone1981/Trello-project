d25dda1976fe7e0800f272a54de0471477857227
Eugene.Lapa <iapa2002@mail.ru>
2023-02-07 20:50:51 +0300

    fix dataUtils

--- src/scripts/modules/renderPanel.js
+++ src/scripts/modules/renderPanel.js
@@ -1,15 +1,15 @@
-function renderPanel(domElements, panelType){
-    const tasksArray = getData();
-    getTasksCount(tasksArray, panelType)
-    switch(panelType){
-        case 'todo':
-            clear(domElements.todo)
-        case 'progress':
-            clear(domElements.progress)
-        case 'done':
-            clear(domElements.done)
-    }
-    arrayCards.forEach(item => {
-        item.renderTaskCard(cardTitle, cardDescription, creationTime,cardUser)
-    });
+function renderPanel (domElements, panelType) {
+  const tasksArray = getData()
+  getTasksCount(tasksArray, panelType)
+  switch (panelType) {
+    case 'todo':
+      clear(domElements.todo)
+    case 'progress':
+      clear(domElements.progress)
+    case 'done':
+      clear(domElements.done)
+  }
+  arrayCards.forEach(item => {
+    item.renderTaskCard(cardTitle, cardDescription, creationTime, cardUser)
+  })
 }
