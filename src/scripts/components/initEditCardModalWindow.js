6b7ec225d10dcdad160bdc3f2da5bd2e4c500935
ArtemiyG <artemijgrinkevic@gmail.com>
2023-02-11 22:21:53 +0300

    modal-fix

--- src/scripts/components/initEditCardModalWindow.js
+++ src/scripts/components/initEditCardModalWindow.js
@@ -8,19 +8,19 @@ export function initEditCardModalWindow (cardId) {
   const arrayUsers = boardObjects[0].usersArray
 
   const modalOverlay = document.createElement('div')
-  modalOverlay.className = 'modal-edit_overlay'
+  modalOverlay.className = 'modal-edit__overlay'
 
   const modalContainer = document.createElement('div')
-  modalContainer.className = 'modal-edit_container'
+  modalContainer.className = 'modal-edit__container'
   modalOverlay.append(modalContainer)
 
   const modalTitle = document.createElement('input')
-  modalTitle.className = 'modal-edit_title'
+  modalTitle.className = 'modal-edit__title'
   modalTitle.placeholder = 'Title'
   modalTitle.type = 'text'
 
   const modalDescription = document.createElement('textarea')
-  modalDescription.className = 'modal-edit_description'
+  modalDescription.className = 'modal-edit__description'
   modalDescription.placeholder = 'Description'
   modalDescription.maxLength = 500
 
@@ -31,13 +31,13 @@ export function initEditCardModalWindow (cardId) {
   fillSelectList(arrayUsers,select)
 
   const modalButtons = document.createElement('div')
-  modalButtons.className = 'modal-edit_buttons'
+  modalButtons.className = 'modal-edit__buttons'
 
   const modalFooter = document.createElement('div')
-  modalFooter.className = 'modal-edit_footer'
+  modalFooter.className = 'modal-edit__footer'
 
-  modalButtons.append(createButton('modal-edit-cancel', 'modal-edit_cancel-btn', 'Cancel', 'button', 'Cancel action'),
-    createButton('modal-edit-confirm', 'modal-edit_confirm-btn', 'Confirm', 'submit', 'Confirm action'))
+  modalButtons.append(createButton('modal-edit-cancel', 'modal-edit__cancel-btn', 'Cancel', 'button', 'Cancel action'),
+    createButton('modal-edit-confirm', 'modal-edit__confirm-btn', 'Confirm', 'submit', 'Confirm action'))
 
   modalFooter.append(select, modalButtons)
 
@@ -51,5 +51,5 @@ export function initEditCardModalWindow (cardId) {
     }
   })
   const root = document.querySelector('#root')
-  root.append(modalOverlay)
+  root.prepend(modalOverlay)
 }
