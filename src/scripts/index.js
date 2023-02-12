97530a905b4597db95d2ca05f1c933b06221e7a3
ArtemiyG <artemijgrinkevic@gmail.com>
2023-02-12 09:39:36 +0300

    modal

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,10 @@ import '../styles/index.scss'
 import { initApp } from './initApp.js'
 // import { createUsersEditWindow } from './components/features/_usersEditWindow.js'
 // import { initEditCardModalWindow } from './components/initEditCardModalWindow.js'
-// import { initConfirmModalWindow } from './components/initConfirmModalWindow.js'
+//  import { initConfirmModalWindow } from './components/initConfirmModalWindow.js'
 const root = document.querySelector('#root')
 
 initApp(root)
 // createUsersEditWindow()
 // initEditCardModalWindow('task-card-14')
-// initConfirmModalWindow('jvghh')
+//  initConfirmModalWindow('Display a message')
