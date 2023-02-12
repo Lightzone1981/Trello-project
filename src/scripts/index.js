12efb35453313a545b83431ed33b560f9a1c64d5
ArtemiyG <artemijgrinkevic@gmail.com>
2023-02-12 20:45:42 +0300

    modal fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,10 +2,10 @@ import '../styles/index.scss'
 import { initApp } from './initApp.js'
 // import { createUsersEditWindow } from './components/usersEditWindow.js'
 // import { initEditCardModalWindow } from './components/editCardModalWindow.js'
-// import { initConfirmModalWindow } from './components/confirmModalWindow.js'
+//  import { initConfirmModalWindow } from './components/confirmModalWindow.js'
 const root = document.querySelector('#root')
 
 initApp(root)
 // createUsersEditWindow()
 // initEditCardModalWindow('14')
-// initConfirmModalWindow('Convey something')
+//  initConfirmModalWindow('Convey something')
