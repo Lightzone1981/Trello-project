a13fe6635ba9491063bdff659f48b87c58f603cc
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2023-02-14 09:14:56 +0300

    webpack copy plugin added

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path')
 
 module.exports = {
@@ -25,7 +26,15 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public/index.html')
     }),
-    new MiniCssExtractPlugin()
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'public/favicon.svg'),
+          to: path.resolve(__dirname, 'dist')
+        },
+      ],
+    }),
   ],
   module: {
     rules: [
