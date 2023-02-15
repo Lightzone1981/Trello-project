28751fbb148c2506ded14c2dea86f787d4016ad0
Eugene.Lapa <iapa2002@mail.ru>
2023-02-15 20:15:29 +0300

    fix

--- src/scripts/handlers/handlerMoveCardForward.js
+++ src/scripts/handlers/handlerMoveCardForward.js
@@ -7,14 +7,24 @@ import { initConfirmModalWindow } from '../components/confirmModalWindow.js'
 export function handlerMoveCardForward(cardId) {
   const boardObjects = getData()
   const arrayCards = boardObjects[0].tasksArray
+  const domElements = getDomElements()
 
   const idNumber = cardId.split('-')[0]
   arrayCards.forEach((item) => {
     if (String(item.id) === String(idNumber)) {
       if (item.type === 'todo') {
         item.type = 'progress'
-      } else if (item.type === 'progress') {
-        item.type = 'done'
+      }
+      else if (item.type === 'progress') {
+        initConfirmModalWindow('Do you want to delete all tasks?')
+        domElements.modalOverlayConfirm.addEventListener('click', (event) => {
+          if (event.target.id === 'modal-confirm-cancel') {
+            domElements.modalOverlay.remove()
+          }
+          if (event.target.id === 'modal-confirm-confirm') {
+             item.type = 'done'
+          }
+        })
       }
 
       setData(boardObjects)
