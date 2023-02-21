1dfc27dacd3aed72b75b0598a63fec4d67e0c8be
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2023-02-21 15:42:24 +0300

    Delete src directory

--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-import '../styles/index.scss'
-import { initApp } from './initApp.js'
-const root = document.querySelector('#root')
-
-initApp(root)
--- src/scripts/modules/renderBoardHeader.js
+++
@@ -1,6 +0,0 @@
-import { getData } from '../utils/dataUtils.js'
-
-export const renderBoardHeader = (domElements) => {
-  const boardObject = getData()
-  domElements.boardTitle.innerText = boardObject.title
-}
