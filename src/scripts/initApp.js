bb3e175dad880360b441536de8fdea6213c36460
Evgeny Lebedevsky <115792378+Lightzone1981@users.noreply.github.com>
2023-02-21 17:38:58 +0300

    hotfix

--- src/scripts/initApp.js
+++ src/scripts/initApp.js
@@ -1,6 +1,6 @@
 import { createMainLayout } from './components/mainLayout.js'
 import { renderAllData } from './renderers/renderAllData.js'
-import { initAllListeners } from './initAllListeners.js'
+import { initAllListeners } from './listeners/initAllListeners.js'
 import { initSoundsElements } from './utils/sounds.js'
 // инициализация приложения
 export function initApp (root) {
